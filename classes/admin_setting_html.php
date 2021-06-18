2796fef1a3b7861834117b3f90ab94e28f42a63f
Tessa Fabry <tessafabry@catalyst-au.net>
2021-06-18 10:17:00 +1000

    wr357702 refactor registration_box template

--- classes/admin_setting_html.php
+++ classes/admin_setting_html.php
@@ -70,9 +70,9 @@ class admin_setting_html extends admin_setting {
         if ($this->hubinfo === false) {
           $this->hubinfo = (object) [];
         }
-        $this->hubinfo->registrationurl = $registrationurl->out(false);
-        $this->hubinfo->register = get_string('contenthub:register', 'hvp');
-        $this->hubinfo->changesettings = get_string('contenthub:changesettings', 'hvp');
+        $this->hubinfo->register = get_string('contenthub:register', 'hvp', $registrationurl);
+        $this->hubinfo->changesettings = get_string('contenthub:changesettings', 'hvp', $registrationurl);
         return $OUTPUT->render_from_template('mod_hvp/content_hub_registration_box', $this->hubinfo);
     }
 }
+
